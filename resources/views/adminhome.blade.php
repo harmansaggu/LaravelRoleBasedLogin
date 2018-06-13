af7da3a6daeb842b8606c5e44e2157f26e072b1f
Harmanpreet Singh <githarry@outlook.com>
2018-06-13 12:54:08 +0530

    first commit

---
+++ resources/views/adminhome.blade.php
@@ -0,0 +1,44 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Dashboard</div>
+
+                    <div class="card-body">
+                        @if (session('status'))
+                            <div class="alert alert-success">
+                                {{ session('status') }}
+                            </div>
+                        @endif
+                        <div class="alert alert-success">
+                            <p>You are Logged in as Admin</p>
+                        </div>
+
+                        <table class="table table-hover table-bordered">
+                            <thead>
+                            <tr>
+                                <th width="5">No.</th>
+                                <th>Member Name</th>
+                                <th>Email</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                                @foreach($users as $key => $value)
+                                    <tr>
+                                        <td>{{ $key+1 }}</td>
+                                        <td>{{ $value->name }}</td>
+                                        <td>{{ $value->email }}</td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+
+
+Auth::routes();
+
+Route::group(['middleware' => ['web','auth']], function (){
+    Route::get('/', function () {
+        return view('welcome');
+    });
+
+    Route::get('/home', function (){
+        if (Auth::user() -> admin == 0){
+            return view('home');
+        }else{
+            $users['users'] = \App\User::all();
+            return view('adminhome', $users);
+        }
+    });
+});
